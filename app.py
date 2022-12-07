b9332c6eed85588b1732f80beabdeb21bf2b5b9a
Albert Tian <atian@berkeley.edu>
2022-12-06 19:05:18 -0800

    content: pushing

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import numpy as np
 import pandas as pd
+from ml import train_model
 
 
 def getInputOutput(df):
@@ -14,14 +15,26 @@ def getInputOutput(df):
             allInputOptions.extend(g[g_key])
     return allInputOptions, allOutputOptions
 
+def train_model_callback(df, inputOptions, outputOption):
+    train_model(df, output=outputOption, inputs=inputOptions)
+    st.write("dogwater")
 
-csv_file = st.file_uploader("Upload a CSV", type=["csv"])
-if (csv_file):
-    df = pd.read_csv(csv_file)
-    number = st.slider("Display how many rows", 0, len(df))
-    st.write(df.head(number))
-    allInputOptions, allOutputOptions = getInputOutput(df)
-    inputOptions = st.multiselect("Pick input features",allInputOptions)
-    outputOptions = st.radio("Pick an output options",allOutputOptions)
-    # st.write(allInputOptions)
-    # st.write(allOutputOptions)
+def main():
+    csv_file = st.file_uploader("Upload a CSV", type=["csv"])
+    if (csv_file):
+        # Read the table
+        df = pd.read_csv(csv_file)
+
+        # Display the table
+        number = st.slider("Display how many rows", 0, len(df))
+        st.write(df.head(number))
+
+        # Select input options
+        allInputOptions, allOutputOptions = getInputOutput(df)
+        inputOptions = st.multiselect("Pick input features",allInputOptions)
+        outputOption = st.radio("Pick an output options",allOutputOptions)
+
+        # Train model
+        st.button("Train model", on_click=train_model_callback, args=(df, inputOptions, outputOption))
+
+main()
